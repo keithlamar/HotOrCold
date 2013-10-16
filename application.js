67492c6d16f1a1f3df4df06b6a49cd2aa300d31a
keithlamar <klgdesign@gmail.com>
2013-10-15 23:20:54 -0400

    Added HTML form

--- application.js
+++ application.js
@@ -1,53 +1,38 @@
-$(document).ready(function(){
-	/*function num(){
-		this.Num = Math.floor(Math.random()*101);
-		this.more20 = this.Num + 20;
-		this.less20 = this.Num - 20;
-		this.less10 = this.Num - 10;
-		this.more10 = this.Num + 10;
-		this.more5 = this.Num + 10;
-		this.less5 = this.Num - 5;
-		
-	};*/
-
-	
-	var userNum = prompt("Choose a number between 1 - 100")
+	var userNum = $('#guess').val();
 	var comNum = Math.floor(Math.random()*101);
-	var difference = function (a,b) {return Math.abs(a - b)}
+	var difference = function (a,b) {return Math.abs(a - b)};
 	var askNumber = function(){
 		if(userNum == comNum){
 			alert("You got it! You Win!")
 		}
 		else if(difference(userNum,comNum) < 5){
-			alert("very hot");
-			userNum = prompt("Try again!")
-			askNumber();
+			alert("Very Hot!");
+			
 		}
 		else if(difference(userNum,comNum) < 10){
-			alert("hot");
-			userNum = prompt("Try again!")
-			askNumber();
+			alert("Hot!");
+			
 		}
 		else if(difference(userNum,comNum) < 20){
-			alert("Getting Warmer");
-			userNum = prompt("Try again!")
-			askNumber();
+			alert("Warm!");
+			
 		}
 		else if(difference(userNum,comNum) > 20){
-			alert("Very cold!")
-			userNum = prompt("Try again")
-			askNumber();
+			alert("Very Cold!")
+					
 		}
-	
 		else {
 			alert("Sorry, you need to choose a number.")
-			userNum = prompt("Try again")
-			askNumber();
+			
 		}
 
-	}
+	};
 
-	
+$(document).ready(function(){
+
+	$('#pick').click(function(){
 	askNumber();
+	});
 
-})
+});
+
