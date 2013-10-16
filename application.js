9ff293698f46ef9f821a98326f3c065b6a4f9f74
keithlamar <klgdesign@gmail.com>
2013-10-16 07:55:58 -0400

    Hotter and colder code added

--- application.js
+++ application.js
@@ -1,33 +1,76 @@
-	var userNum = $('#guess').val();
+	
 	var comNum = Math.floor(Math.random()*101);
-	var difference = function (a,b) {return Math.abs(a - b)};
-	var askNumber = function(){
+	function difference(a,b) {return Math.abs(a - b)};
+	function askNumber(){
+		var userNum = $('#guess').val();
+		var diff = difference(userNum,comNum);
+		var lastNum = Number($("#lastpick li").first().text());
+		var diffLast = difference(lastNum,comNum);
+		function lastpick(number){
+			$("#lastpick").prepend("<li>" + number + "</li>");
+
+		};
 		if(userNum == comNum){
-			alert("You got it! You Win!")
+			alert("You got it! You Win!");
+			lastpick(userNum);
 		}
-		else if(difference(userNum,comNum) < 5){
-			alert("Very Hot!");
-			
+		else if(diff < 5){
+			if (diffLast > diff) {
+				alert("Getting Warmer!")
+				lastpick(userNum);
+			}
+			else if(diffLast < diff){
+				alert("Getting Colder!")
+				lastpick(userNum);
+			}
+			else{
+			alert("Very Hot!");	
+			lastpick(userNum);}
 		}
-		else if(difference(userNum,comNum) < 10){
+		else if(diff < 10){
+			if (diffLast > diff) {
+				alert("Getting Warmer!")
+				lastpick(userNum);
+			}
+			else if(diffLast < diff){
+				alert("Getting Colder!")
+				lastpick(userNum);
+			}
+			else{
 			alert("Hot!");
-			
+			lastpick(userNum);}
 		}
-		else if(difference(userNum,comNum) < 20){
+		else if(diff < 20){
+			if (diffLast > diff) {
+				alert("Getting Warmer!")
+				lastpick(userNum);
+			}
+			else if(diffLast < diff){
+				alert("Getting Colder!")
+				lastpick(userNum);
+			}
+			else{
 			alert("Warm!");
-			
+			lastpick(userNum);}
 		}
-		else if(difference(userNum,comNum) > 20){
+		else if(diff > 20){
+			if (diffLast > diff) {
+				alert("Getting Warmer!")
+				lastpick(userNum);
+			}
+			else if(diffLast < diff){
+				alert("Getting Colder!")
+				lastpick(userNum);
+			}
+			else{
 			alert("Very Cold!")
-					
+			lastpick(userNum);}	
 		}
 		else {
-			alert("Sorry, you need to choose a number.")
-			
+			alert("Sorry, you need to choose a number.")	
 		}
 
 	};
-
 $(document).ready(function(){
 
 	$('#pick').click(function(){
